b09635e3dfc160148ac7a0eda53b031a3e308840
João Pedro Patrocinio <jpedro9966@gmail.com>
2020-11-17 18:50:10 -0300

    Added common folder to build input on build.sh script

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ mkdir BUILD_DIR
 
 # Build server
 echo "Building server executable..."
-g++ src/server/*.cpp -o BUILD_DIR/server
+g++ src/server/*.cpp src/common/*.cpp -o BUILD_DIR/server
 
 # Build client
 echo "Building client executable..."
-g++ src/client/*.cpp -o BUILD_DIR/client
+g++ src/client/*.cpp src/common/*.cpp -o BUILD_DIR/client
